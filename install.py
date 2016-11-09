597f122fba70205b84ccb97e271b9312be0e3e2d
Jonathan Dowland <jdowland@redhat.com>
2016-11-09 10:28:00 +0000

    update examples for eap70-tech-preview-1.2
    patch jboss-eap-7.0.3-patch.zip

--- install.py
+++ install.py
@@ -12,6 +12,6 @@ class Install(JBossInstall):
 
     def install(self):
         self._unpack_distribution()
-        self._apply_patches(["jbeap4410.zip"])
+        self._apply_patches(["patch.zip"])
         self._change_owner()
 
